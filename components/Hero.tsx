efcfa97162fbf2c13f23f4855aeb2430ff16b2b4
Selim Boumedien <selim.boumedien@hotmail.fr>
2023-10-25 15:39:33 +0200

    mise en place du projet avec next, composant navbar et hero en cours de modif , images importée

---
+++ components/Hero.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react'
+import { Flex, Heading, Text, Button } from "@chakra-ui/react"
+    import { format } from 'path'
+import styles from './Hero.module.css'
+import Link from 'next/link'
+
+
+const Hero = () => {
+  return (
+    <div>Hero</div>
+  )
+}
+
+export default Hero
